4a4118ab34a8aa08c0f4436734d5c01aff473d3b
ltsmeyuri <112394310+ltsmeyuri@users.noreply.github.com>
2022-09-02 09:35:56 -0300

    Update questão 3.py

--- questão 3.py
+++ questão 3.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep  2 09:32:30 2022
-
-@author: yurigmf
-"""
-
 a = int(input('Insira um número: '))
 b = int(input('Insira outro número: '))
 print('A soma dos números inseridos é: {}'.format(a+b))
